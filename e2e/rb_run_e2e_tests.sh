f2dc8e8f3e1b87d268e99d199ba03e7806541920
Miguel Alvarez <malvarez@redborder.com>
2025-03-14 13:23:35 +0000

    Fix tests

--- e2e/rb_run_e2e_tests.sh
+++ e2e/rb_run_e2e_tests.sh
@@ -5,18 +5,35 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SCRIPT_PATH="$SCRIPT_DIR/rb_produce_syn_data.sh"
 bash "$SCRIPT_PATH"
 
-echo "Checking if Druid tasks are running..."
+echo "Checking for all Druid supervisors..."
 
-DRUID_TASK_STATUS=$(curl -s -X POST -H "Content-Type: application/json" \
-  -d '{"query": "SELECT * FROM druid_task"}' \
+SUPERVISOR_TASK_STATUS=$(curl -s -X GET "$DRUID_HOST/druid/indexer/v1/supervisor")
+
+echo "Supervisor Task Status:"
+echo "$SUPERVISOR_TASK_STATUS"
+
+if [[ "$SUPERVISOR_TASK_STATUS" == "[]" ]]; then
+  echo "No supervisors found. Exiting with failure."
+  exit 1
+else
+  echo "Supervisor tasks found. Exiting with success."
+  exit 0
+fi
+
+DRUID_TASK_DATA=$(curl -s -o response.json -w "%{http_code}" -X POST -H "Content-Type: application/json" \
+  -d '{"query": "SELECT * FROM rb_flow"}' \
   "$DRUID_HOST/druid/v2/sql")
 
-echo "Druid Query Result:"
-echo "$DRUID_TASK_STATUS"
+HTTP_STATUS="${DRUID_TASK_DATA: -3}"
 
-if echo "$DRUID_TASK_STATUS" | grep -q "RUNNING"; then
-  echo "Druid task is running!"
+echo "HTTP Status: $HTTP_STATUS"
+echo "Druid Query Result:"
+cat response.json
+
+if [ "$HTTP_STATUS" -eq 200 ]; then
+  echo "Druid query successful. Exiting with success."
+  exit 0
 else
-  echo "No running tasks found in Druid!"
+  echo "Druid query failed with status code $HTTP_STATUS. Exiting with failure."
   exit 1
 fi
