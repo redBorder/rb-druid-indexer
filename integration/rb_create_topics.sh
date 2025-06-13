6b9e48325817874e3529e8093a42a506d29db945
vimesa <vimesa@redborder.com>
2025-06-13 14:44:21 +0100

    Fix tests

--- integration/rb_create_topics.sh
+++ integration/rb_create_topics.sh
@@ -7,6 +7,13 @@ TOPICS=("rb_monitor" "rb_flow_post")
 PARTITIONS=1
 REPLICATION_FACTOR=1
 
+# Wait for Kafka to be ready
+echo "Waiting for Kafka to be ready..."
+until docker exec $KAFKA_CONTAINER kafka-topics --bootstrap-server localhost:9092 --list &>/dev/null; do
+  echo "Kafka not ready, waiting..."
+  sleep 5
+done
+
 for TOPIC in "${TOPICS[@]}"; do
   echo "Creating topic: $TOPIC"
 
